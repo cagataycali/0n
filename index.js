5135c05b322e3f6a7d73bbec9419b69f4a7ac12c
cagataycali <calicagatay@icloud.com>
2016-12-07 11:40:02 -0800

    🐛  Bug fix

--- index.js
+++ index.js
@@ -4,54 +4,57 @@ const E = require('3x3c');
 const inquirer = require('inquirer');
 var globalModulesDir = require('global-modules');
 
-// if is online :)
+function q() {
+  var questions = [
+    {
+      type: 'list',
+      name: 'option',
+      message: 'How can I help you,',
+      choices: ['List', 'Add', 'Remove', 'Run worker!', 'Stop worker!'],
+    }
+  ];
+
+  inquirer.prompt(questions).then(function (answers) {
+
+     if (answers.option === 'List') {
+       require('./lib/list')();
+     } else if (answers.option === 'Add') {
+       require('./lib/add')().then(value => {console.log(value);}).catch((err) => {console.log(err);})
+     } else if (answers.option === 'Remove') {
+       require('./lib/remove')().then((value) => {console.log(value);}).catch((err) => {console.log(err);})
+     } else if (answers.option === 'Run worker!') {
+       require('./lib/worker').run();
+     } else if (answers.option == 'Stop worker!') {
+       require('./lib/worker').stop();
+     }
+  });
+}
+
+// Check init
+function c() {
+  return new Promise(function(resolve, reject) {
+    try {
+      var preferences = require(`${process.env['HOME']}/.0n/preferences.json`).mail;
+      if (preferences.username.length === 0 || preferences.password.length === 0 || preferences.to.length === 0 || preferences.from.length === 0) {
+        resolve(true);
+      }
+    } catch (e) {
+      resolve(true);
+    }
+    reject(false);
+  });
+}
 
 E(`${globalModulesDir}/0n/run.sh`)
   .then((value) => {
 
-
-    var init = false;
-    try {
-      var preferences = require(`${process.env['HOME']}/.0n/preferences.json`).mail;
-      if (preferences.username.length < 0 || preferences.password.length < 0 || preferences.to.length < 0 || preferences.from.length < 0) {
-        init = true;
-      }
-    } catch (e) {
-      init = true;
-    }
-
-    if (init) {
-      require('./lib/init')().then((value) => {console.log(value);}).catch((err) => {console.log(err);})
-    } else {
-
-      var questions = [
-        {
-          type: 'list',
-          name: 'option',
-          message: 'How can I help you,',
-          choices: ['List', 'Add', 'Remove', 'Run worker!', 'Stop worker!'],
-        }
-      ];
-
-      inquirer.prompt(questions).then(function (answers) {
-
-         if (answers.option === 'List') {
-           require('./lib/list')();
-         } else if (answers.option === 'Add') {
-           require('./lib/add')().then(value => {console.log(value);}).catch((err) => {console.log(err);})
-         } else if (answers.option === 'Remove') {
-           require('./lib/remove')().then((value) => {console.log(value);}).catch((err) => {console.log(err);})
-         } else if (answers.option === 'Run worker!') {
-           require('./lib/worker').run();
-         } else if (answers.option == 'Stop worker!') {
-           require('./lib/worker').stop();
-         }
-      });
-
-    }
-
-
-
-
+    c()
+      .then((init) => {
+        require('./lib/init')().then((value) => {console.log(value);}).catch((err) => {console.log(err);})
+      })
+      .catch((init) => {
+        console.log(init);
+        q();
+      })
   })
   .catch((err) => {console.log(err);})
